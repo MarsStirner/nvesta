ad408cac1716fc51f85bd4b58a9cde92777d293e
Михаил Мальков <m.malkov@bars-open.ru>
2016-06-17 15:06:43 +0300

    TMIS-1138 Update

--- nvesta/cli.py
+++ nvesta/cli.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
-
 import sys
 
-import itertools
-
 import pymongo
-
 from hitsl_utils.safe import safe_traverse, safe_dict
-from nvesta.library.shape import RefBookRegistry
 from nvesta.library.nsi.client import NsiClient
 from nvesta.library.nsi.data import list_nsi_dictionaries, import_nsi_dict
+from nvesta.library.shape import RefBookRegistry
 
 __author__ = 'viruzzz-kun'
 
@@ -70,12 +66,6 @@ def update_nsi_dicts():
 
 
 def migrate_from_v1():
-    def produce_fd(code):
-        return {
-            'key': code,
-            'mandatory': code == 'code',
-        }
-
     def blocks(iterable, max_size=2000):
         result = []
         i = 0
@@ -88,6 +78,13 @@ def migrate_from_v1():
                 i = 0
         if result:
             yield result
+
+    def get_field_codes(collection):
+        codes = set()
+        for row in collection.find():
+            codes |= set(row.keys())
+        codes.discard('_id')
+        return codes
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--all', action='store_const', const=True, default=False)
@@ -108,21 +105,16 @@ def migrate_from_v1():
     db_vesta = mongo[args.from_db]
     RefBookRegistry.bootstrap(mongo[args.db])
 
+    processed_dicts = {'dict_names'}
+
     for v_description in db_vesta['dict_names'].find():
         code = v_description.get('code')
+        if code in processed_dicts:
+            continue
         v_collection = db_vesta[code]
         count = v_collection.count()
 
         print 'Transferring', str(count).rjust(6), code
-
-        if code == 'dict_names':
-            print '...Skipped'
-            continue
-
-        field_codes = set()
-        for row in v_collection.find():
-            field_codes |= set(row.keys())
-        field_codes.discard('_id')
 
         primary_link = None
         linked = v_description.get('linked')
@@ -141,12 +133,36 @@ def migrate_from_v1():
             'fields': [{
                 'key': fc,
                 'mandatory': fc == 'code',
-            } for fc in sorted(field_codes)],
+            } for fc in sorted(get_field_codes(v_collection))],
             'primary_link': primary_link,
             'version': safe_traverse(v_description, 'version', 'version', default=None)
         })
-        rb.meta.reshape()
 
         for block in blocks(v_collection.find()):
             rb.save_bulk(rb.record_factory(raw) for raw in block)
 
+    for code in db_vesta.collection_names(False):
+        if code in processed_dicts:
+            continue
+
+        v_collection = db_vesta[code]
+        count = v_collection.count()
+
+        print 'Transferring', str(count).rjust(6), code
+
+        rb = RefBookRegistry.create({
+            'code': code,
+            'name': code,
+            'description': '',
+            'oid': '',
+            'fields': [{
+                'key': fc,
+                'mandatory': fc == 'code',
+            } for fc in sorted(get_field_codes(v_collection))],
+            'primary_link': None,
+        })
+
+        for block in blocks(v_collection.find()):
+            rb.save_bulk(rb.record_factory(raw) for raw in block)
+
+    RefBookRegistry.bootstrap(mongo[args.db])
